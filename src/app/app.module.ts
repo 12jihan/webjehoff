60e91ad8a5551a8fd278143a5cc861492a3f71d8
12jikan <jareem.e.hoff@gmail.com>
2023-11-04 01:23:33 -0400

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { SharedModule } from './shared/shared/shared.module';
+import { FeatureModule } from './feature/feature/feature.module';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/shared/shared/shared.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NavbarComponent } from '../components/navbar/navbar.component';
+
+
+
+@NgModule({
+  declarations: [
+    NavbarComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class SharedModule { }
