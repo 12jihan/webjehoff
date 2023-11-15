d1314377ff09483978081d79fe4fc9e6a32b43f2
Jareem Hoff <jareem.e.hoff@gmail.com>
2023-11-14 23:27:39 -0500

    still working on thesanges

--- src/app/shared/components/githubtable/github-table.component.ts
+++ src/app/shared/components/githubtable/github-table.component.ts
@@ -9,33 +9,16 @@ import { GithubService } from 'src/app/core/services/github-service/github.servi
 export class GithubTableComponent implements OnInit {
 
   tableHeaders: string[] = ['Name', 'Area', 'Something', 'Else'];
-  tableContent: { name: string, area: string, something: Number, else: Date }[] = [
-    { 
-      name: 'asd', 
-      area: 'asdasd', 
-      something: 1, 
-      else: new Date() 
-    },
-    { 
-      name: 'asd', 
-      area: 'asdasd', 
-      something: 1, 
-      else: new Date() 
-    },
-    { 
-      name: 'asd', 
-      area: 'asdasd', 
-      something: 1, 
-      else: new Date() 
-    },
-  ]
+  repoContent: any = [];
 
   constructor(private _gh: GithubService) {
 
   }
 
   ngOnInit(): void {
-    this._gh.getRepos();
+    // this.repoContent = this._gh.getRepos().then(repos => {
+    //   this.repoContent = repos;
+    // });
   }
 
 }
