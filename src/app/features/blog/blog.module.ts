89eeedbfe905164d4e448c78c74691905a2f894c
Jareem Hoff <jareem.e.hoff@gmail.com>
2023-11-21 16:53:05 -0500

    more changes

--- src/app/features/blog/blog.module.ts
+++ src/app/features/blog/blog.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { BlogRoutingModule } from './blog-routing.module';
+import { BlogPageComponent } from './components/blog-page/blog-page.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    BlogPageComponent
+  ],
   imports: [
     CommonModule,
     BlogRoutingModule
---
+++ src/app/features/blog/components/blog-page/blog-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'jeh-blog-page',
+  templateUrl: './blog-page.component.html',
+  styleUrl: './blog-page.component.scss'
+})
+export class BlogPageComponent {
+
+}
